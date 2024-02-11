0d5490c03ae1dc2160652a9ee96d17c430914f19
Qendrim Behrami <behrami@in.tum.de>
2024-02-11 22:29:29 +0100

    Replace old weird string manipulation with parser
    The parser is able to evaluate basic mathematical
    expressions involving number and the standard
    operators. Furthermore, testing indicates that
    it handles negative numbers and floating-point
    numbers as well as division by 0.

--- dist/calculator.js
+++ dist/calculator.js
@@ -3,28 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.evaluate = void 0;
 const OPERATOR_REGEX = /[+\-*/]/g;
 const PRECISION = 6; // Precision of decimal numbers
-function evaluate(expression, lastResult = null) {
-    let operands = expression.split(OPERATOR_REGEX);
-    const operators = Array.from(expression.matchAll(OPERATOR_REGEX), (match) => ({
-        operator: match[0],
-        position: match.index,
-    }));
-    // Binary expression
-    if (operands.length == 2) {
-        let a = operands[0];
-        let b = operands[1];
-        switch (operators[0].operator) {
-            case "+":
-                return add(a, b);
-            case "-":
-                return sub(a, b);
-            case "*":
-                return mul(a, b);
-            case "/":
-                return div(a, b);
-        }
-    }
-    return null;
+const parser_1 = require("./parser");
+function evaluate(expression) {
+    expression = expression.replace(/\s/g, ""); //Remove all whitespace
+    let parser = new parser_1.Parser(expression);
+    return fix(parser.parse(), PRECISION);
 }
 exports.evaluate = evaluate;
 /**
@@ -36,27 +19,4 @@ exports.evaluate = evaluate;
 function fix(a, precision) {
     return parseFloat(a.toFixed(precision));
 }
-function add(a, b) {
-    let operand1 = parseFloat(a);
-    let operand2 = parseFloat(b);
-    return fix(operand1 + operand2, PRECISION);
-}
-function sub(a, b) {
-    let operand1 = parseFloat(a);
-    let operand2 = parseFloat(b);
-    return fix(operand1 - operand2, PRECISION);
-}
-function mul(a, b) {
-    let operand1 = parseFloat(a);
-    let operand2 = parseFloat(b);
-    return fix(operand1 * operand2, PRECISION);
-}
-function div(a, b) {
-    let operand1 = parseFloat(a);
-    let operand2 = parseFloat(b);
-    if (operand2 == 0) {
-        console.error("Division by zero!");
-        return NaN;
-    }
-    return fix(operand1 / operand2, PRECISION);
-}
+//# sourceMappingURL=calculator.js.map
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,37 +1,12 @@
 const OPERATOR_REGEX = /[+\-*/]/g;
 const PRECISION = 6; // Precision of decimal numbers
 
-export function evaluate(
-  expression: string,
-  lastResult: number | null = null
-): number | null {
-  let operands = expression.split(OPERATOR_REGEX);
+import { Parser } from "./parser";
 
-  const operators = Array.from(
-    expression.matchAll(OPERATOR_REGEX),
-    (match) => ({
-      operator: match[0],
-      position: match.index,
-    })
-  );
-
-  // Binary expression
-  if (operands.length == 2) {
-    let a = operands[0];
-    let b = operands[1];
-    switch (operators[0].operator) {
-      case "+":
-        return add(a, b);
-      case "-":
-        return sub(a, b);
-      case "*":
-        return mul(a, b);
-      case "/":
-        return div(a, b);
-    }
-  }
-
-  return null;
+export function evaluate(expression: string): number | null {
+  expression = expression.replace(/\s/g, ""); //Remove all whitespace
+  let parser = new Parser(expression);
+  return fix(parser.parse(), PRECISION);
 }
 
 /**
@@ -42,29 +17,4 @@ export function evaluate(
  */
 function fix(a: number, precision: number) {
   return parseFloat(a.toFixed(precision));
-}
-
-function add(a: string, b: string): number {
-  let operand1 = parseFloat(a);
-  let operand2 = parseFloat(b);
-  return fix(operand1 + operand2, PRECISION);
-}
-function sub(a: string, b: string): number {
-  let operand1 = parseFloat(a);
-  let operand2 = parseFloat(b);
-  return fix(operand1 - operand2, PRECISION);
-}
-function mul(a: string, b: string): number {
-  let operand1 = parseFloat(a);
-  let operand2 = parseFloat(b);
-  return fix(operand1 * operand2, PRECISION);
-}
-function div(a: string, b: string): number {
-  let operand1 = parseFloat(a);
-  let operand2 = parseFloat(b);
-  if (operand2 == 0) {
-    console.error("Division by zero!");
-    return NaN;
-  }
-  return fix(operand1 / operand2, PRECISION);
 }
